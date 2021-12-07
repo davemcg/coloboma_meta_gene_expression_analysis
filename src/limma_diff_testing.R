46c517e42ac86504ce99458948107d139160d47c
David McGaughey <mcgaughey@gmail.com>
2021-12-06 21:31:30 -0500

    analysis tweaks, more file saves

--- src/limma_diff_testing.R
+++ src/limma_diff_testing.R
@@ -2,19 +2,19 @@ library(edgeR)
 library(sva)
 library(tidyverse)
 
-load('data/NGS_processing.Rdata')
+load('data/microarray_NGS_objects.Rdata')
 
-ngs_counts <- qsmooth_counts # works best at producing most padj in OF temporal testing (After - During)
+norm_counts <- qsmooth_counts # works best at producing most padj in OF temporal testing (After - During)
 
-sample_meta_D <- sample_meta %>% filter(Sample %in% colnames(ngs_counts)) %>%
+sample_meta_D <- sample_meta %>% filter(Sample %in% colnames(norm_counts)) %>%
   dplyr::select(Sample:Section, Layout:Fusion) %>%
   mutate(S2 = case_when(Section == 'OF' ~ 'OF', TRUE ~ 'OC')) %>%
   unique()
 
 # remove low expression genes from consideration
 cutoff <- 3
-drop <- which(apply((ngs_counts), 1, max) < cutoff) #which(apply(cpm(d_zed), 1, max) < cutoff)
-d <- ngs_counts[-drop,]
+drop <- which(apply((norm_counts), 1, max) < cutoff) #which(apply(cpm(d_zed), 1, max) < cutoff)
+d <- norm_counts[-drop,]
 dim(d) # number of genes left
 
 
@@ -40,7 +40,7 @@ sv_obj <- sva(as.matrix(d_filtered), mm, mod0, n.sv=num_sv)
 ### do NOT use for diff testing
 ### only (maybe) for vis?
 sva_counts <- removeBatchEffect(d_filtered, covariates = sv_obj$sv)
-write_tsv(sva_counts, file = 'data/sva_counts.tsv.gz')
+write_tsv(sva_counts %>% as_tibble(rownames = 'Gene'), file = 'data/sva_counts.tsv.gz')
 
 colnames(sv_obj$sv) <- c('SVA1','SVA2')
 modSv = cbind(mm,sv_obj$sv)
@@ -54,17 +54,17 @@ fit_contrasts <- contrasts.fit(fit, contrast.matrix)
 efit <- eBayes(fit_contrasts)
 top.table_OF_AD <- topTable(efit, sort.by = "p", n = Inf, coef="AfterOF - DuringOF", adjust.method="BH")
 head(top.table_OF_AD, 20)
-write_tsv(top.table_OF_AD, file = 'data/top.table_OF_AD.tsv.gz')
+write_tsv(top.table_OF_AD %>% as_tibble(rownames = 'Gene'), file = 'data/top.table_OF_AD.tsv.gz')
 top.table_OF_AD %>% filter(adj.P.Val<0.05) %>% dim()
 
 top.table_OF_DB <- topTable(efit, sort.by = "p", n = Inf, coef="DuringOF - BeforeOF")
 head(top.table_OF_DB, 20)
-write_tsv(top.table_OF_DB, file = 'data/top.table_OF_DB.tsv.gz')
+write_tsv(top.table_OF_DB %>% as_tibble(rownames = 'Gene'), file = 'data/top.table_OF_DB.tsv.gz')
 top.table_OF_DB %>% filter(adj.P.Val<0.05) %>% dim()
 
 top.table_During <- topTable(efit, sort.by = "p", n = Inf, coef="DuringOC - DuringOF", adjust.method="BH")
 head(top.table_During, 20)
-write_tsv(top.table_During, file = 'data/top.table_OF_OC_During.tsv.gz')
+write_tsv(top.table_During %>% as_tibble(rownames = 'Gene'), file = 'data/top.table_OF_OC_During.tsv.gz')
 top.table_During %>% filter(adj.P.Val<0.05) %>% dim()
 ####################################################################
 
@@ -73,7 +73,7 @@ save(top.table_OF_AD, top.table_OF_DB, top.table_During, file = 'data/top_tables
 ##########
 # create counts by section | stage | paper | tech
 #########
-ngs_counts_merge_tidy <- ngs_counts %>%
+sva_counts_merge_tidy <- sva_counts %>%
   as_tibble(rownames = 'Gene') %>%
   pivot_longer(-Gene, names_to = 'Sample', values_to = 'log2(norm counts)') %>%
   left_join(sample_meta_D) %>%
@@ -81,6 +81,6 @@ ngs_counts_merge_tidy <- ngs_counts %>%
   group_by(Gene, Group) %>%
   summarise(`log2(norm counts)` = mean(`log2(norm counts)`))
 
-ngs_counts_merge <- ngs_counts_merge_tidy %>%
+sva_counts_merge <- sva_counts_merge_tidy %>%
   pivot_wider(names_from = Group, values_from = `log2(norm counts)`)
-write_tsv(ngs_counts_merge,'data/ngs_counts_merge.tsv.gz')
+write_tsv(sva_counts_merge %>% as_tibble(rownames = 'Gene'),'data/sva_counts_merge.tsv.gz')
